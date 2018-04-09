4cfdd6586a19bb029870d03aec882f4fc9325ad1
G-UK <34664733+G-UK@users.noreply.github.com>
2018-04-09 23:13:21 +0100

    Added 1 Day Cache
    Thanks to @tomasbrod

--- resources/head.php
+++ resources/head.php
@@ -19,3 +19,10 @@
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="resources/icons/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
+<?php
+    $seconds_to_cache = 86400;
+    $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+    header("Expires: $ts");
+    header("Pragma: cache");
+    header("Cache-Control: max-age=$seconds_to_cache");
+?>
