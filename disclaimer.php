1e3e74d4dfbe1a5b7f7d5833663f6476cea0c080
G-UK <34664733+G-UK@users.noreply.github.com>
2018-04-09 22:59:34 +0100

    Correct Typo
    Thanks @Barton26

--- disclaimer.php
+++ disclaimer.php
@@ -13,7 +13,7 @@
     <p>Last updated: April 06, 2018</p>
     <p>The information contained on https://gridcoin.ddns.net website (the "Service") is for general information purposes only.</p>
     <p>Gridcoin Projects assumes no responsibility for errors or omissions in the contents on the Service.</p>
-    <p>In no event shall Dridcoin Projects be liable for any special, direct, indirect, consequential, or incidental damages or any damages whatsoever, whether in an action of contract, negligence or other tort, arising out of or in connection with the use of the Service or the contents of the Service. Gridcoin Projects reserves the right to make additions, deletions, or modification to the contents on the Service at any time without prior notice.</p>
+    <p>In no event shall Gridcoin Projects be liable for any special, direct, indirect, consequential, or incidental damages or any damages whatsoever, whether in an action of contract, negligence or other tort, arising out of or in connection with the use of the Service or the contents of the Service. Gridcoin Projects reserves the right to make additions, deletions, or modification to the contents on the Service at any time without prior notice.</p>
     <p>Gridcoin Projects does not warrant that the website is free of viruses or other harmful components.</p>
     <p>This Disclaimer for https://gridcoin.ddns.net is powered by TermsFeed</p>
     <h2>External links disclaimer</h2>
