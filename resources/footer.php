8b58a97476fb229f11356306c7719ce25f28e0a1
G-UK <34664733+G-UK@users.noreply.github.com>
2018-04-20 22:43:17 +0100

    Moved to Standard Sections

--- resources/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="col-12">
-    Copyright © 2018 G_UK<br>
-    <a href="privacy_policy.php">Privacy Policy</a><br>
-    <a href="cookies_policy.php">Cookies Policy</a><br>
-    <a href="terms.php">Terms &amp; Conditions</a><br>
-    <a href="disclaimer.php">Disclaimer</a><br> 
-</footer>
