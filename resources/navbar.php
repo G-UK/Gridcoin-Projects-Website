463043b8d2b693554a064d07079810b519754e60
G-UK <34664733+G-UK@users.noreply.github.com>
2018-04-20 22:44:18 +0100

    Moved to Standard Sections

--- resources/navbar.php
+++
@@ -1,9 +0,0 @@
-<topnav>
-    <div class="col-1 col-s-2 col-m-2 navlink"><a href="index.php">Home</a></div>
-    <div class="col-1 col-s-2 col-m-2 navlink"><a href="project_listings.php">The List</a></div>
-    <div class="col-1 col-s-2 col-m-2 navlink"><a href="listing_process.php">The Process</a></div>
-    <div class="col-1 col-s-2 col-m-2 navlink"><a href="under_construction.php">The Projects</a></div>
-    <div class="col-1 col-s-2 col-m-2 navlink"><a href="under_construction.php">Useful Links</a></div>
-    <div class="col-1 col-s-2 col-m-2 navlink navlink-s"><a href="under_construction.php">Resources</a></div>
-    <div class="col-6 col-s-12 col-m-12 navcomment"> Please Note: Site Under Development, Not all features are available yet.</div>
-</topnav>
