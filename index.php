4b2e63af0da32bf7ad0f208cb79b643fefb6bc4d
G-UK <34664733+G-UK@users.noreply.github.com>
2018-04-20 22:34:09 +0100

    Multiple changes

--- index.php
+++ index.php
@@ -1,30 +1,33 @@
 <!doctype html>
 <html lang="en-GB">
 <head>
-    <?php include('resources/head.php');?>
+    <?php include('/srv/www/gridcoin.ddns.net/resources/standard-sections/head.php');?>
+    <?php include('/srv/www/gridcoin.ddns.net/resources/standard-sections/24hr-cache.php');?>
     <title>Gridcoin Projects Home</title>
 </head>
 
 <body>
-<?php include('resources/header.php');?>
-<?php include('resources/navbar.php');?>
-<article class="col-12 col-s-12 col-m-12"> 
-    <div class="col-9 col-s-12 col-m-6">
-        <h2>What is Gridcoin?</h2>
+
+<!-- Header -->
+<?php include('/srv/www/gridcoin.ddns.net/resources/standard-sections/header.php');?>
+<?php include('/srv/www/gridcoin.ddns.net/resources/standard-sections/navbar.php');?>
+
+<!-- Left Sidebar -->
+<?php include('/srv/www/gridcoin.ddns.net/resources/standard-sections/project-sidebar.php');?>
+
+<!-- Main Column -->
+<article class="col-one">
+    <div>
+        <h2>What is Gridcoin?</h2><a href="https://gridcoin.us"><img src="/resources/images/GRCHorizontal.png" alt="gridcoin logo" class="grcimage"></a>
         <p>Gridcoin is an open-source blockchain that mints and distributes cryptocurrency in relation to the processing power a network participant directs toward data-driven analysis and scientific discovery. Currently, the Gridcoin blockchain is secured through a proof-of-stake protocol and monitors processing contributions to the distributed computing infrastructure, BOINC.
         <br><br>
         To read more about Gridcoin visit the official site
         <br>
         <a href="https://gridcoin.us">https://gridcoin.us</a></p>
     </div>
-    <div class="col-3 col-s-12 col-m-6">
-        <a href="https://gridcoin.us">
-            <img src="resources/images/GRCHorizontal.png" alt="gridcoin logo" class="rightbodyimg1"></a>
-    </div>
-</article>
-<article class="col-12 col-s-12 col-m-12">
-    <div class="col-9 col-s-12 col-m-6">
-        <h2>What is BOINC?</h2>
+
+    <div>
+        <h2>What is BOINC?</h2><a href="https://boinc.berkeley.edu/"><img src="/resources/images/boinc_logo_3d.png" alt="boinc logo" class="boincimage"></a>
         <p>The Berkeley Open Infrastructure for Network Computing (BOINC) is an open-source distributed computing infrastructure which provides anyone with data the opportunity to process that data for near zero cost by utilizing a global volunteer-based distributed computing network.
         <br><br>
         BOINC hosts major institutional computing projects such as IBM’s World Community Grid, SETI, and data from the Large Hadron Collider, alongside projects developed by students, enthusiasts, mathematicians, researchers, and citizen scientists.
@@ -33,19 +36,19 @@
         <br>
         <a href="https://boinc.berkeley.edu/">https://boinc.berkeley.edu/</a></p>
     </div>
-    <div class="col-3 col-s-12 col-m-6">
-        <a href="https://boinc.berkeley.edu/">
-            <img src="resources/images/boinc_logo_3d.png" alt="boinc logo" class="rightbodyimg2"></a>
-    </div>
-</article>
-<article class="col-12 col-s-12 col-m-12">
-    <div class="col-12 col-s-12 col-m-12">
-        <h2 >What are the Gridcoin Listings?</h2>
+
+    <div>
+    <h2>What are the Gridcoin Listings?</h2>
         <p>The Gridcoin listings were created to provide fair rewards, increase security and to focus user's resources on active projects. The essence of the listing processes are that projects must meet certain requirements regarding work availability, fair distribution of work and security.</p>
-        <p><a href="listing_process.php">Read the current Gridcoin Project Listing Process</a></p>
-        <p><a href="project_listings.php">See the current Gridcoin Project Listings</a></p>
     </div>
 </article>
-<?php include('resources/footer.php');?>
+
+<!-- Right Sidebar -->
+<aside class=right>
+</aside>
+
+<!-- Footer -->
+<?php include('/srv/www/gridcoin.ddns.net/resources/standard-sections/footer.php');?>
+
 </body>
 </html>
