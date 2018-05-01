e198b6169ce85232d8729ad22b12d2637159c243
G-UK <34664733+G-UK@users.noreply.github.com>
2018-05-01 03:11:34 +0100

    Fix piwik analytic img and cleanup old classes

--- resources/standard-sections/header.php
+++ resources/standard-sections/header.php
@@ -1,10 +1,10 @@
-<header class="col-12">
-    <div class="col-3 col-s-12">
+<header>
+    <div>
         <img src="/resources/images/gridcoin_blocks.png" alt="gridcoin logo"/>
     </div>
-    <div class="col-7 col-s-12">
+    <div>
         <h1 class="heading">Gridcoin Projects</h1>
     </div>
     <div class="col-2"></div>
-    <noscript><p><img src="https://gridcoin.ddns.net/piwik/piwik.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+    <noscript><p><img src="https://gridcoin.ddns.net:8062/piwik/piwik.php?idsite=1&amp;rec=1" alt="" /></p></noscript>
 </header>
