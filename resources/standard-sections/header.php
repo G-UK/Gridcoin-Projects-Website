d8bbcf9a559c5c2cfe3bd49299a2d05e33be9e4d
G-UK <34664733+G-UK@users.noreply.github.com>
2018-05-01 17:23:54 +0000

    Added Titles to Images

--- resources/standard-sections/header.php
+++ resources/standard-sections/header.php
@@ -1,10 +1,20 @@
 <header>
     <div>
-        <img src="/resources/images/gridcoin_blocks.png" alt="gridcoin logo"/>
+        <img
+             src="/resources/images/gridcoin_blocks.png"
+             alt="Join Gridcoin"
+             title="Gridcoin Logo with Background Cubes"
+         >
     </div>
     <div>
         <h1 class="heading">Gridcoin Projects</h1>
     </div>
     <div class="col-2"></div>
-    <noscript><p><img src="https://gridcoin.ddns.net:8062/piwik/piwik.php?idsite=1&amp;rec=1" alt="" /></p></noscript>
+    <noscript>
+        <p><img
+            src="https://gridcoin.ddns.net:8062/piwik/piwik.php?idsite=1&amp;rec=1"
+            alt="Matomo Analytics Empty Image, Please Read our Privacy Policy"
+            title="Matomo Analytics Empty Image"
+        /></p>
+    </noscript>
 </header>
