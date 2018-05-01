f16f2b2adc7661f8486a839fa7c5e39182c8f33e
G-UK <34664733+G-UK@users.noreply.github.com>
2018-05-01 03:33:53 +0100

    Remove illegal characters from sort URLs

--- pages/project-list.php
+++ pages/project-list.php
@@ -3,7 +3,7 @@
 <head>
     <?php include('/srv/www/gridcoin.ddns.net/resources/standard-sections/head.php');?>
     <?php include('/srv/www/gridcoin.ddns.net/resources/standard-sections/1hr-cache.php');?>
-    <title>Gridcoin Project Listings</title>
+<title>Gridcoin Project Listings</title>
     <script src="/resources/scripts/colour_projectlist.js"></script>
 </head>
 
@@ -18,74 +18,74 @@ echo
     "<tr>
     <th class=\"col-name\">
         <div>Project Name</div>
-        <div><a href=\"?orderBy=`Project Name` ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
-        <a href=\"?orderBy=`Project Name` DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
+        <div><a href=\"?column=ProjectName&orderBy=ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
+        <a href=\"?column=ProjectName&orderBy=DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
     </th>
     <th class=\"col-votes\">
         <div><div class=\"tooltip\">Vote (In/Out)
             <span class=\"tooltiptext\">Result of last Whitelist Vote</span>
         </div></div>
-        <div><a href=\"?orderBy=`Vote (In/Out)` ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
-        <a href=\"?orderBy=`Vote (In/Out)` DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
+        <div><a href=\"?column=Vote(In/Out)&orderBy=ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
+        <a href=\"?column=Vote(In/Out)&orderBy=DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
     </th>
     <th class=\"col-votes\">
         <div><div class=\"tooltip\">Vote (Date)
             <span class=\"tooltiptext\">Date of last Whitelist Vote.<br>Where Applicable</span>
         </div></div>
-        <div><a href=\"?orderBy=`Vote (Date)` ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
-        <a href=\"?orderBy=`Vote (Date)` DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
+        <div><a href=\"?column=Vote(Date)&orderBy=ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
+        <a href=\"?column=Vote(Date)&orderBy=DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
     </th>
     <th class=\"col-credit\">
         <div><div class=\"tooltip\">Total Credit
             <span class=\"tooltiptext\">Total Credit issued by the Project</span>
         </div></div>
-        <div><a href=\"?orderBy=`Project Total Credit` ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
-        <a href=\"?orderBy=`Project Total Credit` DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
+        <div><a href=\"?column=ProjectTotalCredit&orderBy=ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
+        <a href=\"?column=ProjectTotalCredit&orderBy=DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
     </th>
     <th class=\"col-stat\">
         <div><div class=\"tooltip\">Speed (GFlops)
             <span class=\"tooltiptext\">Reported Compute Speed of Project<br>Where Available</span>
         </div></div>
-        <div><a href=\"?orderBy=`Project Compute Speed (GFlops)` ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
-        <a href=\"?orderBy=`Project Compute Speed (GFlops)` DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
+        <div><a href=\"?column=ProjectComputeSpeed(GFlops)&orderBy=ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
+        <a href=\"?column=ProjectComputeSpeed(GFlops)&orderBy=DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
     </th>
     <th class=\"col-stat\">
         <div><div class=\"tooltip\">W.A.S
             <span class=\"tooltiptext\">Work Availability Score =<br>7 Day Avg Daily Credit /<br>40 Day Avg Daily Credit</span>
         </div></div>
-        <div><a href=\"?orderBy=`W.A.S (Work Availability Score)` ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
-        <a href=\"?orderBy=`W.A.S (Work Availability Score)` DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
+        <div><a href=\"?column=W.A.S&orderBy=ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
+        <a href=\"?column=W.A.S&orderBy=DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
     </th>
     <th class=\"col-stat\">
         <div><div class=\"tooltip\">Z.C.D
             <span class=\"tooltiptext\">Number Days of Zero Credit reported by the Project<br>aka Zero Credit Days</span>
         </div></div>
-        <div><a href=\"?orderBy=`Z.C.D (Zero Days Credit)` ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
-        <a href=\"?orderBy=`Z.C.D (Zero Days Credit)` DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
+        <div><a href=\"?column=Z.C.D&orderBy=ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
+        <a href=\"?column=Z.C.D&orderBy=DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
     </th>
     <th class=\"col-list\">
         <div><div class=\"tooltip\">Current Status
             <span class=\"tooltiptext\">Current Whitelist Status</span>
         </div></div>
-        <div><a href=\"?orderBy=`Current Status` ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
-        <a href=\"?orderBy=`Current Status` DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
+        <div><a href=\"?column=CurrentStatus&orderBy=ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
+        <a href=\"?column=CurrentStatus&orderBy=DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
     </th>
     <th class=\"col-list\">
         <div><div class=\"tooltip\">Calculated Status
             <span class=\"tooltiptext\">Calculated Status Using Currently Approved Process</span>
         </div></div>
-        <div><a href=\"?orderBy=`Calculated Status` ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
-        <a href=\"?orderBy=`Calculated Status` DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
+        <div><a href=\"?column=CalculatedStatus&orderBy=ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
+        <a href=\"?column=CalculatedStatus&orderBy=DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
     </th>
     <th class=\"col-update\">
         <div><div class=\"tooltip\">Last Update
             <span class=\"tooltiptext\">Timestamp from last project stats file downloaded</span>
         </div></div>
-        <div><a href=\"?orderBy=`Last Update` ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
-        <a href=\"?orderBy=`Last Update` DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
-    </th class=\"col-note\">
-    <th><div>Notes</div>
-    <div><div></div></th>
+        <div><a href=\"?column=LastUpdate&orderBy=ASC\"><img src=\"/resources/images/sort-up.png\" alt=\"up\"></a>
+        <a href=\"?column=LastUpdate&orderBy=DESC\"><img src=\"/resources/images/sort-down.png\" alt=\"down\"></a></div>
+    <th class=\"col-note\">
+    <div>Notes</div>
+    <div></div></th>
     </tr>";
 
 class TableRows extends RecursiveIteratorIterator {
@@ -96,32 +96,32 @@ class TableRows extends RecursiveIteratorIterator {
 
 $servername = "localhost";
 $username = "php";
-$password = "xxxxxxxxxxx";
+$password = "#qea%vLK3LZw";
 $dbname = "grc_listings";
-$orderBy = array(
-    '`Project Name` ASC',
-    '`Project Name` DESC',
-    '`Vote (In/Out)` ASC',
-    '`Vote (In/Out)` DESC',
-    '`Vote (Date)` ASC',
-    '`Vote (Date)` DESC',
-    '`Project Total Credit` ASC',
-    '`Project Total Credit` DESC',
-    '`Project Compute Speed (GFlops)` ASC',
-    '`Project Compute Speed (GFlops)` DESC',
-    '`W.A.S (Work Availability Score)` ASC',
-    '`W.A.S (Work Availability Score)` DESC',
-    '`Z.C.D (Zero Credit Days)` ASC',
-    '`Z.C.D (Zero Credit Days)` DESC',
-    '`Current Status` ASC',
-    '`Current Status` DESC',
-    '`Calculated Status` ASC',
-    '`Calculated Status` DESC',
-    '`Last Update` ASC',
-    '`Last Update` DESC');
+$column = array(
+    'ProjectName',
+    'Vote(In/Out)',
+    'Vote(Date)',
+    'ProjectTotalCredit',
+    'ProjectComputeSpeed(GFlops)',
+    'W.A.S',
+    'Z.C.D',
+    'CurrentStatus',
+    'CalculatedStatus',
+    'LastUpdate');
 
 $order = '`Project Name` ASC';
-if (isset($_GET['orderBy']) && in_array($_GET['orderBy'], $orderBy)) {$order = $_GET['orderBy'];}  
+if ($_GET['column'] === 'ProjectName') {$columns="`Project Name`";}
+if ($_GET['column'] === 'Vote(In/Out)') {$columns="`Vote (In/Out)`";}
+if ($_GET['column'] === 'Vote(Date)') {$columns="`Vote (Date)`";}
+if ($_GET['column'] === 'ProjectTotalCredit') {$columns="`Project Total Credit`";}
+if ($_GET['column'] === 'ProjectComputeSpeed(GFlops)') {$columns="`Project Compute Speed (GFlops)`";}
+if ($_GET['column'] === 'W.A.S') {$columns="`W.A.S (Work Availability Score)`";}
+if ($_GET['column'] === 'Z.C.D') {$columns="`Z.C.D (Zero Credit Days)`";}
+if ($_GET['column'] === 'CurrentStatus') {$columns="`Current Status`";}
+if ($_GET['column'] === 'CalculatedStatus') {$columns="`Calculated Status`";}
+if ($_GET['column'] === 'LastUpdate') {$columns="`Last Update`";}
+if (isset($_GET['column']) && in_array($_GET['column'], $column)) {$order = $columns . ' ' . $_GET['orderBy'];}  
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
