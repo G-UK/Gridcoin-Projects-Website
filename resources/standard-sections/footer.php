0220cb145bd3d075e150f3009b744250c150d58d
G-UK <34664733+G-UK@users.noreply.github.com>
2018-05-01 02:57:22 +0100

    Add Thanks and Techs

--- resources/standard-sections/footer.php
+++ resources/standard-sections/footer.php
@@ -1,7 +1,62 @@
-<footer class="col-12">
-    Copyright © 2018 G_UK<br>
-    <a href="/pages/legals/privacy-policy.php">Privacy Policy</a><br>
-    <a href="/pages/legals/cookies-policy.php">Cookies Policy</a><br>
-    <a href="/pages/legals/terms.php">Terms &amp; Conditions</a><br>
-    <a href="/pages/legals/disclaimer.php">Disclaimer</a><br> 
+<footer class=fullwidth>
+    <div class=foot1>
+    <!-- Thanks -->
+        <strong>Gridcoin Projects would like to Thank</strong><br>
+        Favicons Provided by: <a href="https://www.favicon-generator.org">Favicon Generator</a><br>
+        Icons Provided by: <a href="https://www.iconsdb.com">IconsDB</a><br>
+        ... And everyone in the wonderful Gridcoin and BOINC Communities!!<br>
+    </div>
+    <div class=foot2>
+        <strong>Copyright © 2018 G_UK<br></strong>
+        <a href="/pages/legals/privacy-policy.php">Privacy Policy</a><br>
+        <a href="/pages/legals/cookies-policy.php">Cookies Policy</a><br>
+        <a href="/pages/legals/terms.php">Terms &amp; Conditions</a><br>
+        <a href="/pages/legals/disclaimer.php">Disclaimer</a><br>
+    </div>
+    <div class=foot3>
+    <!-- Powered By Icons -->
+    <table>
+        <tr>
+            <td rowspan=2>
+            <a href="https://www.apache.org">
+                <img src="/resources/images/apache.png"
+                    alt="Powered by Apache" class=large /></a>
+            </td>
+            <td rowspan=2>
+            <a href="https://varnish-cache.org">
+                <img src="/resources/images/varnish.png"
+                    alt="Powered by Varnish Cache" class=large /></a>
+            </td>
+            <td>
+            <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fgridcoin.ddns.net%2F">
+                <img src="/resources/images/W3C_HTML5.png"
+                    alt="Valid HTML5!" class=small /></a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            <a href="https://jigsaw.w3.org/css-validator/check/referer">
+                <img src="/resources/images/vcss.png"
+                    alt="Valid CSS!" class=small /></a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            <a href="https://www.raspberrypi.org"> 
+                <img src="/resources/images/pipower.png"
+                    alt="Powered by Raspberry Pi" class=small /></a>
+            </td>
+            <td>
+            <a href="https://secure.php.net">
+                <img src="/resources/images/php.png"
+                    alt="Powered by PHP" class=small /></a>
+            </td>
+            <td>
+            <a href="http://www.w3.org/WAI/WCAG2A-Conformance">
+                <img src="/resources/images/wcag2A.png"
+                    alt="Level A conformance, W3C WAI Web Content Accessibility Guidelines 2.0" class=small></a>
+            </td>
+        </tr>
+    </table>
+    </div>
 </footer>
