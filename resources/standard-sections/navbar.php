97e55e1bd11461f905441e49e9c449f6f59bd95e
G-UK <34664733+G-UK@users.noreply.github.com>
2018-05-01 03:13:50 +0100

    New Navbar design with dropdowns

--- resources/standard-sections/navbar.php
+++ resources/standard-sections/navbar.php
@@ -1,9 +1,34 @@
-<topnav>
-    <div class="col-1 col-s-2 navlink"><a href="/index.php">Home</a></div>
-    <div class="col-1 col-s-2 navlink"><a href="/pages/project-list.php">List</a></div>
-    <div class="col-1 col-s-2 navlink"><a href="/pages/project-list-process.php">Process</a></div>
-    <div class="col-1 col-s-2 navlink"><a href="/pages/projects/project-index.php">Projects</a></div>
-    <div class="col-1 col-s-2 navlink"><a href="/pages/links.php">Links</a></div>
-    <div class="col-1 col-s-2 navlink navlink-s"><a href="/pages/under-construction.php">Resources</a></div>
-    <div class="col-6 col-s-12 navcomment">First 7 Project Pages Live!!</div>
-</topnav>
+<div class=topnav>
+    <a class=navbox href="/index.php">Home</a>
+    <div class=dropdown>Listings
+        <button class=dropbtn aria-label="Whitelist Dropdown">
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class=dropdown-content>
+            <a href="/pages/project-list.php">Production Whitelist</a>
+	        <a href="/pages/project-list-v2.php">Development Whitelist</a>
+        </div>
+    </div>
+    <div class=dropdown>Projects
+        <button class="dropbtn" aria-label="Projects Dropdown">
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class=dropdown-content>
+            <a href="/pages/projects/amicable.php">Amicable Numbers</a>
+	        <a href="/pages/projects/asteroids.php">Asteroids@Home</a>
+	        <a href="/pages/projects/acoustics.php">Acoustics@Home</a>
+            <a href="/pages/projects/cas.php">CAS@Home</a>
+            <a href="/pages/projects/csg.php">Citizen Science Grid</a>
+            <a href="/pages/projects/cpdn.php">ClimatePrediction.net</a>
+	        <a href="/pages/projects/collatz.php">Collatz Conjecture</a>
+	        <a href="/pages/projects/cosmology.php">Cosmology@Home</a>
+	        <a href="/pages/projects/denis.php">DENIS</a>
+        	<a href="/pages/projects/numberfields.php">Numberfields</a>
+            <a href="/pages/projects/rosetta.php">Rosetta@Home</a>
+            <a href="/pages/projects/seti.php">SETI@Home</a>
+            <a href="/pages/projects/skynet.php">Skynet:POGS</a>
+        </div>
+    </div>
+    <a class=navbox href="/pages/project-list-process.php">Process</a>
+    <a class=navbox href="/pages/links.php">Links</a>
+</div>
