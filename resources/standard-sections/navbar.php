67b627cf0082a25e841c2f23f8b011b933b0b8d3
G-UK <34664733+G-UK@users.noreply.github.com>
2018-05-02 21:20:56 +0100

    Remoce redundant button and rename div

--- resources/standard-sections/navbar.php
+++ resources/standard-sections/navbar.php
@@ -1,18 +1,12 @@
-<div class=topnav>
-    <a class=navbox href="/index.php">Home</a>
+<topnav>
+    <a href="/index.php">Home</a>
     <div class=dropdown>Listings
-        <button class=dropbtn aria-label="Whitelist Dropdown">
-            <i class="fa fa-caret-down"></i>
-        </button>
         <div class=dropdown-content>
             <a href="/pages/project-list.php">Production Whitelist</a>
 	        <a href="/pages/project-list-v2.php">Development Whitelist</a>
         </div>
     </div>
     <div class=dropdown>Projects
-        <button class="dropbtn" aria-label="Projects Dropdown">
-            <i class="fa fa-caret-down"></i>
-        </button>
         <div class=dropdown-content>
             <a href="/pages/projects/amicable.php">Amicable Numbers</a>
 	        <a href="/pages/projects/asteroids.php">Asteroids@Home</a>
@@ -29,6 +23,6 @@
             <a href="/pages/projects/skynet.php">Skynet:POGS</a>
         </div>
     </div>
-    <a class=navbox href="/pages/project-list-process.php">Process</a>
-    <a class=navbox href="/pages/links.php">Useful Links</a>
-</div>
+    <a href="/pages/project-list-process.php">Process</a>
+    <a href="/pages/links.php">Useful Links</a>
+</topnav>
